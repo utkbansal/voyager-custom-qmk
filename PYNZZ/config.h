b257642c33aaf01d0a2bfe89c64585d90e53ae68
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2026-01-13 14:36:58 +0530

    🔄(qmk): Rename Aq5yZ to PYNZZ for Oryx account integration
    Updated layout ID to match the one saved in Oryx account.

--- PYNZZ/config.h
+++ PYNZZ/config.h
@@ -1,5 +1,5 @@
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "Aq5yZ/6aplYE"
+#define SERIAL_NUMBER "PYNZZ/9Dp3aj"
 #define LAYER_STATE_8BIT
 #define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
 
